b47a80b6ae04af6da19c79838b1a4fa0bd54999b
Said <s.lotus69@gmail.com>
2020-03-25 09:49:14 +0100

    error handling

--- app.py
+++ app.py
@@ -17,13 +17,22 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.get_json()['description']
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return jsonify({
-    'description': todo.description
-  })
+  error = False
+  body = {}
+  try :
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if not error:
+   return jsonify(body)
 
 
 @app.route('/')
