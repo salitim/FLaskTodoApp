ae174c6b91be3da9b1d6cfeaa3bd25d1c38e5393
Said <s.lotus69@gmail.com>
2020-03-26 16:17:27 +0100

    implement delete button

--- app.py
+++ app.py
@@ -49,6 +49,18 @@ def set_completed_todo(todo_id):
       session.close()
     return redirect(url_for['index'])
 
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+      todo = Todo.query.get(todo_id)
+      db.session.delete(todo)
+      db.session.commit()
+    except:
+      db.session.rollback()
+    finally:
+      session.close()
+    return jsonify({ 'success' : True })
+
 @app.route('/')
 def index():
   return render_template('index.html', data=Todo.query.order_by('id').all())
