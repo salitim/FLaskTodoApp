85a1a74ea93332196a31fc5c186007a3fb328345
Said <s.lotus69@gmail.com>
2020-03-31 11:08:10 +0200

    adding list crud delete funcitonnal and new migration

--- app.py
+++ app.py
@@ -23,11 +23,13 @@ class TodoList(db.Model):
   __tablename__ = 'todolists'
   id = db.Column(db.Integer, primary_key = True)
   name = db.Column(db.String(), nullable=False)
-  todos = db.relationship('Todo', backref='list', lazy = True)
+  todos = db.relationship('Todo', backref='list', lazy = True, cascade="all, delete-orphan")
+  completed = db.Column(db.Boolean, nullable=False, default=False)
 
   def __repr__(self):
         return f'<TodoList {self.id} {self.name}>'
     
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -82,8 +84,55 @@ def delete_todo(todo_id):
 def get_list_todos(list_id):
   return render_template('index.html', 
   active_list = TodoList.query.get(list_id),
-  lists = TodoList.query.all(), 
+  lists = TodoList.query.order_by('id').all(),
   todos = Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
+@app.route('/lists/create', methods=['POST'])
+def create_todo_list():
+  error = False
+  body = {}
+  try :
+    name = request.get_json()['name']
+    todo_list = TodoList(name=name)
+    todo_list.completed = False
+    db.session.add(todo_list)
+    db.session.commit()
+    body['id'] = todo_list.id
+    body['name'] = todo_list.name
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if not error:
+   return jsonify(body)
+
+@app.route('/<list_id>/set-completed', methods=['POST'])
+def set_completed_list(list_id):
+    try:
+      completed = request.get_json()['completed']
+      print(completed)
+      list = TodoList.query.get(list_id)
+      list.completed = completed
+      db.session.commit()
+    except:
+      db.session.rollback()
+    finally:
+      dbsession.close()
+    return redirect(url_for('index'))
+
+@app.route('/lists/<list_id>', methods=['DELETE'])
+def delete_list(list_id):
+    try:
+      list = TodoList.query.get(list_id)
+      db.session.delete(list)
+      db.session.commit()
+    except:
+      db.session.rollback()
+    finally:
+      db.session.close()
+    return jsonify({ 'success' : True })
 
 @app.route('/')
 def index():
